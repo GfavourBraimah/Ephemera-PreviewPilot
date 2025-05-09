3b4288c8960229867903ba019d9a9b233e5fd3f1
GfavourBraimah <gfbraimah@gmail.com>
2025-05-09 12:02:52 +0100

    Fixed file path

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,7 @@
+aws_region           = "us-east-1" # Replace with your preferred region
+ecr_repository_name  = "my-furniture-repo" # Replace with your repository name
+ecs_cluster_name     = "my-furniture-cluster"
+ecs_task_family      = "my-furniture-task"
+ecs_service_name     = "my-furniture-service"
+image_tag            = "latest"
+project_name         = "my-furniture"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,42 +1,7 @@
-variable "aws_region" {
-  description = "The AWS region where resources will be created"
-  type        = string
-  default     = "us-east-1" # Replace with your preferred region
-}
-
-variable "ecr_repository_name" {
-  description = "Name of the ECR repository"
-  type        = string
-  default     = "my-furniture-repo" # Replace with your repository name
-}
-
-variable "ecs_cluster_name" {
-  description = "Name of the ECS cluster"
-  type        = string
-  default     = "my-furniture-cluster"
-}
-
-variable "ecs_task_family" {
-  description = "ECS Task family name"
-  type        = string
-  default     = "my-furniture-task"
-}
-
-variable "ecs_service_name" {
-  description = "Name of the ECS service"
-  type        = string
-  default     = "my-furniture-service"
-}
-
-variable "image_tag" {
-  description = "Docker image tag"
-  type        = string
-  default     = "latest"
-}
-
-
-variable "project_name" {
-  description = "Name of the project"
-  type        = string
-  default     = "my-furniture"
-}
+variable "aws_region" {}
+variable "ecr_repository_name" {}
+variable "ecs_cluster_name" {}
+variable "ecs_task_family" {}
+variable "ecs_service_name" {}
+variable "image_tag" {}
+variable "project_name" {}
