680a877d329e7ab6fb86fca8e2eb3b14709f47d6
GfavourBraimah <gfbraimah@gmail.com>
2025-05-16 06:27:16 +0100

    Added files

--- terraform/ecr/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  description = "AWS region where the ECR repository will be created"
-  default     = "us-east-1"
-}
-
-variable "repository_name" {
-  description = "Name of the ECR repository"
-  default     = "my-app-repo"
-}
