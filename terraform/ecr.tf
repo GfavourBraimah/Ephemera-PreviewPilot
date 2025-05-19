b9366f2d86206fcb014f9bcfa84c9693e901c7bf
GfavourBraimah <gfbraimah@gmail.com>
2025-05-19 14:51:57 +0100

    Fixed errors

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,5 +1,6 @@
 resource "aws_ecr_repository" "my_furniture_repo" {
   name                 = var.ecr_repository_name
+  force_delete          = true
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
